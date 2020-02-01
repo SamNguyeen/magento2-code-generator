0e5127192ce033367f3a7f22b328a9baee4a037c
Cuong Nguyen Van <cuongnv@magenest.com>
2020-02-01 22:02:25 +0700

    Rework module template #done

--- templates/module/registration.php
+++ templates/module/registration.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright © 2020 Magenest. All rights reserved.
+ * See COPYING.txt for license details.
+ *
+ * Magenest_${CommentsCompanyName} extension
+ * NOTICE OF LICENSE
+ *
+ * @category Magenest
+ * @package Magenest_${CommentsCompanyName}
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     '${Vendorname}_${Modulename}',
